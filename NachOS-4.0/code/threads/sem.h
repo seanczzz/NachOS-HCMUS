f1d096de5ecf7b62a2531279bfd89eeb41adc40b
SeanC <lehuynhcuong0701@gmail.com>
2024-01-03 10:46:18 +0700

    modify addrspace, add sem.h, pcb.h, stable.h, ptable.h

---
+++ NachOS-4.0/code/threads/sem.h
@@ -0,0 +1,23 @@
+#ifndef SEM_H
+#define SEM_H
+
+class Semaphore;
+
+class Sem
+{
+private:
+  Semaphore *sem;
+  char name[50];
+
+public:
+  Sem(char *na, int i);
+  ~Sem();
+
+  void wait();
+
+  void signal();
+
+  char *GetName();
+};
+
+#endif
